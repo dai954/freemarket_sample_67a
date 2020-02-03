1510988b86f7cbc549579fd78252c60223baeadf
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-02-03 20:21:56 +0900

    商品購入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,29 @@
 class ItemsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+    
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def purchase
+  end
+
 end
