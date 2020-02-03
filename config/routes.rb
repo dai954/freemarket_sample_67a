df13995f9e3f7c592c105c217b4d87fb08417ee4
syota0612 <syo.ta06.12b.s@gmail.com>
2020-02-03 18:49:03 +0900

    add mypage credit front

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   root "items#index"
 
   resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :users, only: [:index, :show] do
+    collection do
+      get :logout
+      get :card
+      get :add
+    end
+  end
 end
