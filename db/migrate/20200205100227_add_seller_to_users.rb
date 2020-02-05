87d55fd3563fb26968826fba2f1a55bbb5a48298
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-02-05 19:18:08 +0900

    外部キー

---
+++ db/migrate/20200205100227_add_seller_to_users.rb
@@ -0,0 +1,5 @@
+class AddSellerToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :users, :seller, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_043437) do
+ActiveRecord::Schema.define(version: 2020_02_05_101359) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "prefecture"
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 2020_02_04_043437) do
     t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "buyer_id"
+    t.bigint "seller_id"
+    t.bigint "category_id"
+    t.bigint "user_id"
+    t.bigint "brand_id"
+    t.index ["brand_id"], name: "index_items_on_brand_id"
+    t.index ["buyer_id"], name: "index_items_on_buyer_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["seller_id"], name: "index_items_on_seller_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "sellers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -104,9 +114,13 @@ ActiveRecord::Schema.define(version: 2020_02_04_043437) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "seller_id"
+    t.bigint "buyer_id"
+    t.index ["buyer_id"], name: "index_users_on_buyer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["seller_id"], name: "index_users_on_seller_id"
   end
 
   add_foreign_key "addresses", "users"
@@ -114,4 +128,11 @@ ActiveRecord::Schema.define(version: 2020_02_04_043437) do
   add_foreign_key "comments", "users"
   add_foreign_key "credits", "users"
   add_foreign_key "images", "items"
+  add_foreign_key "items", "brands"
+  add_foreign_key "items", "buyers"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "sellers"
+  add_foreign_key "items", "users"
+  add_foreign_key "users", "buyers"
+  add_foreign_key "users", "sellers"
 end
