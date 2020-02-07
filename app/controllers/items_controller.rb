b7fc8a6be568b3332028821373ab0f610ee54c64
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-02-07 18:02:48 +0900

    商品詳細表示３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def show
@@ -24,9 +25,14 @@ class ItemsController < ApplicationController
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
   end
 
   def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path, notice: "商品「#{item.name}」を削除しました。"
   end
 
   def purchase
