c2debc49f22feb70b8541d4888c50bac7eea0322
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-02-11 18:27:59 +0900

    差異の修正

--- db/schema.rb
+++ db/schema.rb
@@ -112,11 +112,9 @@ ActiveRecord::Schema.define(version: 2020_02_10_073317) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "seller_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["seller_id"], name: "index_users_on_seller_id"
   end
 
   add_foreign_key "addresses", "users"
