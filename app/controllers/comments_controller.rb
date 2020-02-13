993c8095242cd12458f59b66c455d659d8403b2b
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-02-13 21:03:51 +0900

    aaa

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
+    @item = Item.find(params[:item_id])
     if @comment.save
       Comment.create(comment_params)
       redirect_to item_path(@item)
     else
       flash[:notice] = "コメント欄が空白です。"
-      @item = Item.find(params[:item_id])
       redirect_to item_path(@item)
     end
-
-    @item = Item.find(params[:item_id])
-    redirect_to item_path(@item)
   end
 
   private
