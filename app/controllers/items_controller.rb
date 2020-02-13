c77bc668d8137298d7701e961abddfc782bffa39
hide0902 <hn.tt.sf.hu@icloud.com>
2020-02-13 16:19:53 +0900

    Revert "1"
    This reverts commit 01466c30a6e94aad47a1176adb66cd229d0da703.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,6 @@ class ItemsController < ApplicationController
   end
 
   def update    
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       flash[:notice] = "編集しました。"
       redirect_to
