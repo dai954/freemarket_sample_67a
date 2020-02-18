8d2d4c44d2319ee17506ec560103806989eb13d7
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-02-18 16:43:52 +0900

    完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   #deviseのデフォルトバリデーションはemail,passwordのみのため、追加しています
   validates :name,:ruby_first,:ruby_last,:first_name,:last_name,:birthday, presence: true
-  has_many :credits
-  has_many :items
-  has_many :addresses
-  has_many :comments
+  has_many :credits,dependent: :destroy
+  has_many :items,dependent: :destroy
+  has_many :addresses,dependent: :destroy
+  has_many :comments,dependent: :destroy
   belongs_to :buyer, optional: true
   belongs_to :seller,optional: true,dependent: :destroy
 end
--- db/migrate/20200207032647_change_datatype_status_of_items.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeStatusOfItems < ActiveRecord::Migration[5.2]
-  def change
-    change_column :items, :status, :string
-  end
-end
